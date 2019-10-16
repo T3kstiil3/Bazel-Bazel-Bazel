e634f666fa36f80c8fb90dfd7868aea07234f3b0
Aurélien Loyer <aur.loy@gmail.com>
2019-10-16 13:05:14 +0200

    feat: add submodule and log on bazel build

--- project/bazel-angular-migration/src/app/app.component.ts
+++ project/bazel-angular-migration/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'bazel-angular-migration';
+  title = "bazel-angular-migration";  
+
+  constructor() {
+    console.log('test 2');
+  }
 }
